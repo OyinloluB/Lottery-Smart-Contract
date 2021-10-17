831f53c6de428f5ac52f980417cf1000a1f14d64
OyinloluB <oyinloluwab@gmail.com>
2021-10-17 20:56:52 +0100

    added correct details

--- deploy.js
+++ deploy.js
@@ -3,22 +3,21 @@ const Web3 = require("web3");
 const { interface, bytecode } = require("./compile");
 
 const provider = new HDWalletProvider(
-  "juice bicycle seek common shield hello below angry source share exact mobile",
+  "type job olive correct damp vote fold layer lumber list garment enable",
   // remember to change this to your own phrase!
-  "https://rinkeby.infura.io/v3/15c1d32581894b88a92d8d9e519e476c"
-  // remember to change this to your own endpoint!
+  "https://rinkeby.infura.io/v3/b74b7fcce44e42af8b71dfad0e7a71a9"
 );
 const web3 = new Web3(provider);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
 
-  console.log("Attempting to deploy from account", accounts[0]);
-
   const result = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode })
-    .send({ gas: "1000000", from: accounts[0] });
-
+    .send({ gas: '1000000', gasPrice: '5000000000', from: accounts[0] });
+    
+  console.log(interface);
   console.log("Contract deployed to", result.options.address);
 };
+
 deploy();
